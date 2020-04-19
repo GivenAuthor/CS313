53b26b32ef5d4bac7a7f418b0f4d57bed262ecef
Jared <jaredgreen10@gmail.com>
2020-04-19 14:51:52 -0400

    added homepage

--- web/homepage.php
+++ web/homepage.php
@@ -1,28 +1,11 @@
+<html>
+<body>
+
+Hello
 <?php
-    function openGoogle() {
-        header("Location: www.google.com")
-    }
+    $name = $_GET["name"];
+    echo $name;
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <link rel="stylesheet" type="text/css" href="homepage.css">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jared Green</title>
-</head>
-<body>
-    <h1>Jared Green's Homepage</h1>
-    <hr>
-    <div class="hasButton">
-    <input type="button" value="Assignments" onclick="window.location.href = 'homework.html'">
-    </div>
-    </form>
-    <p>My name is Jared Green, I am a 19 year old student majoring in software engineering. 
-        I am from the Detroit are of Michigan.</p>
-    <div id="hasButton2">
-        <input type="button" value="To Google" onclick=openGoogle()>
-    </div>
 </body>
-</html>
+</html
