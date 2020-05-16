0b4a0e4a3ed65f7ae79854c54aab922944e0c12b
Jared <jaredgreen10@gmail.com>
2020-05-16 14:16:54 -0400

    database.sql added

---
+++ myDb.sql
@@ -0,0 +1,29 @@
+CREATE TABLE account_password(
+	password_id serial PRIMARY KEY,
+	password_contents VARCHAR (30) NOT NULL
+);
+
+CREATE TABLE user(
+	user_id serial PRIMARY KEY,
+	username VARCHAR (50) UNIQUE NOT NULL,
+	CONSTRAINT username_id_fkey FOREIGN KEY (user_id)
+	REFERENCES account_password (password_id)
+
+);
+
+CREATE TABLE note(
+	note_id serial PRIMARY KEY,
+	note_contents STRING
+);
+
+CREATE TABLE rating(
+	rating_id serial PRIMARY KEY,
+	day_rating integer NOT NULL
+);
+
+CREATE TABLE day(
+	day_id serial PRIMARY KEY,
+	day_date DATE,
+	CONSTRAINT day_id_fkey FOREIGN KEY (day_id)
+	REFERENCES note (note_id)
+);
