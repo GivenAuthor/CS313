219b34369615a1205aa3fb5d24bf79c5d76210f4
Jared <jaredgreen10@gmail.com>
2020-06-04 14:23:24 -0400

    submits to db

---
+++ web/project/submitted.php
@@ -0,0 +1,64 @@
+<?php
+echo("
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Home</title>
+    <h3>Thank you for submitting</h3>
+    <form method='POST' action='home.php'>
+    <button type='submit'>To homepage</button>
+    </form>
+");
+
+echo("Submit called");
+    $date = $_POST[date];
+    $rate = $_POST[rating];
+    $note = $_POST[note];
+
+    // connect
+    try
+    {
+        // REPLACE THIS WITH ENTERED INFO
+      $user = 'postgres';
+      $password = 'password';
+      $db = new PDO('pgsql:host=localhost;dbname=postgresql-fitted-13004', $user, $password);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
+    
+    // insert the date
+    try {
+        $sql = "INSERT INTO day (day_date) VALUES ($date)";
+        $conn->exec($sql);
+        echo "date added";
+        }
+    catch(PDOException $e) {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+    
+    // insert the rating for the day
+    try {
+        $sql = "INSERT INTO rating (day_rating) VALUES ($rating)";
+        $conn->exec($sql);
+        echo "rating aded";
+        }
+    catch(PDOException $e) {
+         echo $sql . "<br>" . $e->getMessage();
+    }
+
+    // insert the notes for the day
+    try {
+        $sql = "INSERT INTO note (note_contents) VALUES ($note)";
+        $conn->exec($sql);
+        echo "note added";
+        }
+        catch(PDOException $e) {
+            echo $sql . "<br>" . $e->getMessage();
+        }
+    echo('<p>Submitted</p>');
+?>
