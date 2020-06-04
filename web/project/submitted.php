022711a9745391a2efe955887d07242e598ea3c0
Jared <jaredgreen10@gmail.com>
2020-06-04 19:13:45 -0400

    connect

--- web/project/submitted.php
+++ web/project/submitted.php
@@ -23,7 +23,7 @@ echo("Submit called");
         // REPLACE THIS WITH ENTERED INFO
       $user = 'postgres';
       $password = '1Wickles';
-      $db = new PDO('pgsql:host=localhost port=5432 dbname=postgresql user=postgres password=1Wickles');
+      $db = new PDO("pgsql:host=localhost;port=5432;dbname=postgresql", $user, $password);
     }
     catch (PDOException $ex)
     {
