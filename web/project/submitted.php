73c3f8dc4feaa7803fc304e341cfd2907e200c62
Jared <jaredgreen10@gmail.com>
2020-06-05 15:58:44 -0400

    changed query

--- web/project/submitted.php
+++ web/project/submitted.php
@@ -29,7 +29,7 @@ echo("Submit called");
         // REPLACE THIS WITH ENTERED INFO
       $user = 'jared';
       $password = '1Wickles';
-      $db = new PDO('pgsql:host=$dbHost;port=$dbPort;dbname=$dbName', $user, $password);
+      $db = new PDO('pgsql:host=localhost;port=5432;dbname=postgresql-fitted-13004', $user, $password);
     }
     catch (PDOException $ex)
     {
