3bc0172b261ce2b66e775e321db12e01244d8178
Jared <jaredgreen10@gmail.com>
2020-06-06 11:42:31 -0400

    display results

--- web/project/request.php
+++ web/project/request.php
@@ -1,17 +1,4 @@
 <?php
-echo("
-<!DOCTYPE html>
-<html lang='en'>
-<head>
-    <meta charset='UTF-8'>
-    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-    <title>Info</title>
-</head>
-<body>
-    <h1>Previous day's data</h1>
-</body>
-</html>
-");
     // get dates, ratings, and notes
         $stmt = $this->pdo->query('SELECT * FROM day');
         $days = $stmt->fetchAll(PDO::FETCH_ASSOC);
