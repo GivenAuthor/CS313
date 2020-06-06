89fbdb3648c72e70664aa294e763ac3594c7abf9
Jared <jaredgreen10@gmail.com>
2020-06-06 12:05:41 -0400

    request info

--- web/project/request.php
+++ web/project/request.php
@@ -1,12 +1,30 @@
 <?php
+echo("
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <meta charset='UTF-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title>Info</title>
+</head>
+<body>
+    <h1>Previous day's data</h1>
+</body>
+</html>
+");
     // get dates, ratings, and notes
+    $query = $db->prepare('SELECT * FROM day');
+    $query->execute();
+    $days = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($days);
+
         $stmt = $this->pdo->query('SELECT * FROM day');
         $days = $stmt->fetchAll(PDO::FETCH_ASSOC);
         echo($days);
 
         $stmt1 = $this->pdo->query('SELECT * FROM rating WHERE rating_id = day_id');
         $rating = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo("<p>Hello</p>");
+        echo("<br>");
         echo($rating);
 
         $stmt1 = $this->pdo->query('SELECT * FROM note WHERE note_id = note_id');
